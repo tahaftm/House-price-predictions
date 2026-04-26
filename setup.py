74b0baa6748c76daf369cd58a3c678d9d8577a34
Taha Tariq <tahatariqf@gmail.com>
2026-04-26 08:25:29 -0700

    initial Setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+HYPHEN_E_DOT = '-e .'
+def find_requirements(file):
+    with open(file) as f:
+        requirements = f.readlines()
+    requirements = [req.replace("\n","") for req in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    return requirements 
+
+
+setup(
+    name="HousePricePredictions",
+    author="TahaTariq",
+    author_email="tahatariqf@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    requires=find_requirements('requirements.txt')
+)
