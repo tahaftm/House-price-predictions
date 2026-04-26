460f63a450b7dbf1b70809b71c159bcab89cd12a
Taha Tariq <tahatariqf@gmail.com>
2026-04-26 14:03:45 -0700

    Outliers removed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -17,6 +17,21 @@ class DataIngestion:
 
         os.makedirs("artifacts", exist_ok=True)
 
+        num_df = train_df.select_dtypes(exclude="object")
+
+        Q1 = num_df.quantile(0.25)
+        Q3 = num_df.quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower = Q1 - 1.5 * IQR
+        upper = Q3 + 1.5 * IQR
+
+        outlier_rows = num_df[((num_df < lower) | (num_df > upper)).any(axis=1)]
+
+        train_df.drop(outlier_rows.index, axis = 0,inplace=True)
+
+        train_df = train_df.dropna(subset=['MasVnrArea', 'Electrical', 'BsmtExposure'])
+
         train_df.to_csv(self.data_ingestion_config.training_data_path)
         test_df.to_csv(self.data_ingestion_config.testing_data_path)
 
